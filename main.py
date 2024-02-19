171719f6aa0522614ad600b74f9efe0ac4e5d1b3
EduardoAquinta <edwoodashton@yahoo.co.uk>
2024-02-19 16:49:21 +0000

    create methods within the code

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import pandas as pd
 import gcsfs
 import google.auth
-# from Data import correct_list
 import yaml
 
+# Global variable declarations
 bucket_name = 'eduardo_data_hold'
 file_name = 'data.csv'
-
 credentials, project_id = google.auth.default(scopes=['https://www.googleapis.com/auth/devstorage.read_only'])
 
 
+# Import the checklist
 def get_checklist():
     with open("Data/correct_list.yaml", "r") as data:
         output = yaml.safe_load(data)
         return output["Checklist"]
 
 
+# Import the data from GCS
 def get_data():
     fs = gcsfs.GCSFileSystem(project='My First Project',
                              token=credentials)
@@ -24,13 +25,14 @@ def get_data():
         return output
 
 
+# Variables for the data Checker
 df = get_data()
 checklist = get_checklist()
-
 list_of_column_names = list(df.columns)
 
 print("list_of_column_names: ", list_of_column_names)
 print("checklist: ", checklist)
+
 
 for element in checklist:
     if element not in list_of_column_names:
